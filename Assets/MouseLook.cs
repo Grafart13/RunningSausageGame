74933584c6bc75050b6e23d64336e7c0ecf214da
Wojciech Baszczyk <wbaszczyk@gmail.com>
2016-04-13 00:35:15 +0200

    simple movement

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public float lookSenvitivity = 5f;
+	public float xRotation;
+	public float yRotation;
+	public float currentXRotation;
+	public float currentYRotation;
+	public float xRotationVelocity;
+	public float yRotationVelocity;
+	public float lookSmoothDamp = 0.1f;
+	// Use this for initialization
+	void Start () {
+		xRotation=0;
+		yRotation=0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xRotation -= Input.GetAxis("Mouse Y") * lookSenvitivity;
+        yRotation += Input.GetAxis("Mouse X") * lookSenvitivity;
+        
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationVelocity, lookSmoothDamp);
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationVelocity, lookSmoothDamp);
+        transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+    public GameObject camera01;
+
+    public float force;
+    public float xForce;
+    public float yForce;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(0, camera01.GetComponent<MouseLook>().currentYRotation, 0);
+
+        xForce = Input.GetAxis("Horizontal") * force;
+        yForce = Input.GetAxis("Vertical") * force;
+        Debug.Log(xForce);
+
+        transform.Translate(new Vector3(xForce, 0, yForce));
+    }
+}
