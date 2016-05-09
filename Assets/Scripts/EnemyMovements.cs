e3ab51b8469e9d899d980ed6618e6215a7598d43
Grafart13 <dpjarosz@gmail.com>
2016-05-09 22:20:25 +0200

    Adding running enemy

---
+++ Assets/Scripts/EnemyMovements.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovements : MonoBehaviour {
+
+    Transform player; // referencja do pozycji playera
+    //PlayerHealth playerHealth; // referencja do zmiennej trzymającej zdrowie Parowki
+    NavMeshAgent nav;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetCOmponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);	
+	}
+}
