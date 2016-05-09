966066fb703ba0574b47a480df78b18514959889
Grafart13 <dpjarosz@gmail.com>
2016-05-10 00:23:07 +0200

    Adding PlayerHealth and Enemy attack skill

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    bool playerInRange; // czy Parowka weszla w kolidera wroga
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        playerHealth = player.GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true; // Parowka weszla w kolidera wroga!
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player )
+        {
+            playerInRange = false; // Parowki juz nie ma w zasiegu kolidera wroga!
+            anim.ResetTrigger("Attack");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+        
+        if ((timer>=timeBetweenAttacks) && (playerInRange))
+        {
+            Attack(); // ATAKUJ!
+        }
+
+        if (playerHealth.currentHealth <=0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+	}
+
+    void Attack()
+    {
+        timer = 0f;
+
+        if (playerHealth.currentHealth > 0)
+        {
+            anim.SetTrigger("Attack");
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    movement playerMovement;
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<movement>();
+        currentHealth = startingHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+	}
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+        playerMovement.enabled = false;
+    }
+
+}
